ffa180076b15d742cfbd8f837a234cf4cdb954f3
Andriy Deshko <cappertone@gmail.com>
2019-11-14 01:34:35 +0200

    do some refactoring

--- src/com/epam/autumn/politech/HT1/GuessingGame.java
+++ src/com/epam/autumn/politech/HT1/GuessingGame.java
@@ -5,32 +5,41 @@ import java.util.Scanner;
 public class GuessingGame {
     private static int count = 0;
     private static final int RANDOMNUMBER = 1 + (int)(Math.random() * 1000.0D);
+    private static boolean guessed = false;
 
-    public GuessingGame() {
-    }
 
     public static void main(String[] args) {
         System.out.println("Input number from 1 to 1000");
         Scanner sc = new Scanner(System.in);
+        System.out.println(RANDOMNUMBER);
+        doGame(sc);
+        sc.close();
+    }
 
-        while(true) {
-            while(sc.hasNextInt()) {
+    private static void doGame(Scanner sc) {
+        while(!guessed) {
+            if(sc.hasNextInt()) {
                 int number = sc.nextInt();
                 if (number >= 1 && number <= 1000) {
                     System.out.println(compare(number));
                 } else {
                     System.out.println("inputData out of range");
                 }
+            }else if (!sc.hasNextInt()){
+                System.out.println("wrong inputData");
+                break;
             }
-
-            sc.close();
-            return;
         }
     }
 
     private static String compare(int number) {
         ++count;
-        String win = String.format("win with the %s attempt, guessed number is %s", count, RANDOMNUMBER);
-        return number < RANDOMNUMBER ? "Too Low" : (number == RANDOMNUMBER ? win : "Too High");
+        return number < RANDOMNUMBER ? "Too Low" : (number == RANDOMNUMBER ? win() : "Too High");
     }
+
+    private static String win(){
+        guessed = true;
+        return String.format("win with the %s attempt, guessed number is %s", count, RANDOMNUMBER);
+    }
+
 }
